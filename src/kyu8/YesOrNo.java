f03ff3075987dfb9124f8d8d25b473467bdfa66b
xxSlashxx <roland.speckmeier@gmx.de>
2018-10-14 16:53:30 +0200

    Added new Kata

---
+++ src/kyu8/YesOrNo.java
@@ -0,0 +1,18 @@
+package kyu8;
+
+/**
+ * Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+ */
+public class YesOrNo
+{
+    public static void main(String[] args)
+    {
+        System.out.println(boolToWord(true));
+        System.out.println(boolToWord(false));
+    }
+
+    public static String boolToWord(boolean b)
+    {
+        return b ? "Yes" : "No";
+    }
+}
