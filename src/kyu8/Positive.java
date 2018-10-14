f0fb4735cb9c9af330c339a5c325a8d8c282f4ce
xxSlashxx <roland.speckmeier@gmx.de>
2018-10-14 16:50:12 +0200

    Added new Kata

---
+++ src/kyu8/Positive.java
@@ -0,0 +1,33 @@
+package kyu8;
+
+/**
+ * You get an array of numbers, return the sum of all of the positives ones.
+ * Example [1,-4,7,12] => 1 + 7 + 12 = 20
+ * Note: if there is nothing to sum, the sum is default to 0.
+ */
+public class Positive
+{
+    public static void main(String[] args)
+    {
+        System.out.println(sum(new int[]{1, 2, 3, 4, 5}));
+        System.out.println(sum(new int[]{1, -2, 3, 4, 5}));
+        System.out.println(sum(new int[]{}));
+        System.out.println(sum(new int[]{-1, -2, -3, -4, -5}));
+        System.out.println(sum(new int[]{-1, 2, 3, 4, -5}));
+    }
+
+    public static int sum(int[] arr)
+    {
+        int sum = 0;
+
+        for (int value : arr)
+        {
+            if (value > 0)
+            {
+                sum += value;
+            }
+        }
+
+        return sum;
+    }
+}
