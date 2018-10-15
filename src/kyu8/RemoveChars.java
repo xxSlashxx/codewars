bdc17ebc7f70b53fe47991b06fdb12d5040e001a
xxSlashxx <roland.speckmeier@gmx.de>
2018-10-15 17:50:54 +0200

    Added new Kata

---
+++ src/kyu8/RemoveChars.java
@@ -0,0 +1,19 @@
+package kyu8;
+
+/**
+ * It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string.
+ * You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+ */
+public class RemoveChars
+{
+    public static void main(String[] args)
+    {
+        System.out.println(remove("eloquent"));
+        System.out.println(remove("country"));
+    }
+
+    public static String remove(String str)
+    {
+        return str.substring(1, str.length() - 1);
+    }
+}
