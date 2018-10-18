94f665a613bdc2e69215f888e02d87e1928df04d
xxSlashxx <roland.speckmeier@gmx.de>
2018-10-18 18:39:36 +0200

    Added new Kata

---
+++ src/kyu5/ProdFib.java
@@ -0,0 +1,47 @@
+package kyu5;
+
+/**
+ * The Fibonacci numbers are the numbers in the following integer sequence (Fn):
+ * 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, ...
+ * such as
+ * F(n) = F(n-1) + F(n-2) with F(0) = 0 and F(1) = 1.
+ * Given a number, say prod (for product), we search two Fibonacci numbers F(n) and F(n+1) verifying
+ * F(n) * F(n+1) = prod.
+ * Your function productFib takes an integer (prod) and returns an array:
+ * [F(n), F(n+1), true] or {F(n), F(n+1), 1} or (F(n), F(n+1), True)
+ * depending on the language if F(n) * F(n+1) = prod.
+ * If you don't find two consecutive F(m) verifying F(m) * F(m+1) = prodyou will return
+ * [F(m), F(m+1), false] or {F(n), F(n+1), 0} or (F(n), F(n+1), False)
+ * F(m) being the smallest one such as F(m) * F(m+1) > prod.
+ * Examples
+ * productFib(714) # should return {21, 34, 1},
+ * # since F(8) = 21, F(9) = 34 and 714 = 21 * 34
+ * productFib(800) # should return {34, 55, 0},
+ * # since F(8) = 21, F(9) = 34, F(10) = 55 and 21 * 34 < 800 < 34 * 55
+ * Notes: Not useful here but we can tell how to choose the number n up to which to go: we can use the "golden ratio" phi which is (1 + sqrt(5))/2 knowing that F(n) is asymptotic to: phi^n / sqrt(5). That gives a possible upper bound to n.
+ */
+public class ProdFib
+{
+    public static void main(String[] args)
+    {
+        System.out.println(productFib(4895)); // => {55, 89, 1}
+    }
+
+    public static long[] productFib(long prod)
+    {
+        long f1 = 1;
+        long f2 = 1;
+        long sum = f1 + f2;
+        long myProd = f1 * f2;
+
+        while (myProd < prod)
+        {
+            f1 = f2;
+            f2 = sum;
+            sum = f1 + f2;
+            myProd = f1 * f2;
+        }
+
+        return new long[]{f1, f2, myProd == prod ? 1 : 0};
+    }
+}
